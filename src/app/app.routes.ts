9cd7b0e4200358b5ae6942919e5ac4da9ec2060e
Krzysztof Jagoda <krzysztof.jagoda99@gmail.com>
2024-05-15 18:20:20 +0200

    Added redirect for empty route and 404 page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MemoGameComponent } from './memo-game/memo-game.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   {
@@ -11,4 +12,6 @@ export const routes: Routes = [
     path: 'memo-game',
     component: MemoGameComponent,
   },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
 ];
